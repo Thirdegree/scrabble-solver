2b3744d631930b751aa15db72f173ad95aa09fb0
Joel Christiansen <greenbadge.jc@gmail.com>
2023-06-01 21:11:23 +0200

    binding to 127.0.0.1 is a good way to make sure nobody uses my thing

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ pub mod solver;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let mut server = api::Server::bind("127.0.0.1:1984").await?;
+    let mut server = api::Server::bind("0.0.0.0:1984").await?;
     server.run().await;
     Ok(())
 }
